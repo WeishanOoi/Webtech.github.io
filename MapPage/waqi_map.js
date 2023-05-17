5b99a0ffc04fa30cfb83ba311e15f6c107fbdabe
Ooi Weishan <130117362+WeishanOoi@users.noreply.github.com>
2023-05-17 23:15:08 +0800

    Update waqi_map.js

--- MapPage/waqi_map.js
+++ MapPage/waqi_map.js
@@ -1,15 +1,13 @@
-      function  initBingMap()  {  
-  
-            var  map  =  new  Microsoft.Maps.Map(document.getElementById('map'),  {      
-                        center:  new  Microsoft.Maps.Location(51.505,  -0.09),    
-                        zoom:11,  
-            }  );  
-  
-            var  options  =  {    
-                  uriConstructor:  "https://tiles.aqicn.org/tiles/usepa-aqi//{zoom}/{x}/{y}.png?token=_TOKEN_ID_",  
-                  minZoom:  1,  
-                  maxZoom:  15  
-            };  
-            var  waqiTileSource  =  new  Microsoft.Maps.TileSource(options);  
-            var  waqiTilelayer  =  new  Microsoft.Maps.TileLayer({  mercator:  waqiTileSource  });  
-            map.layers.insert(waqiTilelayer);  
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                 '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                 'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(mymap);
+
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
