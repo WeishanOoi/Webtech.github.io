e20cc91d8f70df99a3b75fb138ba389232e07484
Ooi Weishan <130117362+WeishanOoi@users.noreply.github.com>
2023-05-17 11:21:23 +0800

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,9 +1,7 @@
-"use strict"
-let map;
-
-function getMap(){
-    map = new Microsoft.Maps.Map('#map', {
-        // You need your key.
-    credentials: 'AmCdGM5M9Px2QVxkUld09IDlK3M2gKajjbYpCm58wbeelDCZscodbbgGT1xv_vgV',
-    });
-};
+function UseBingMap() { 
+ 
+    var map = new Microsoft.Maps.Map(document.getElementById('map'), { 
+        center: new Microsoft.Maps.Location(3.140853, 101.693207), 
+        zoom:11, 
+        credentials: 'AmCdGM5M9Px2QVxkUld09IDlK3M2gKajjbYpCm58wbeelDCZscodbbgGT1xv_vgV',
+    } ); 
