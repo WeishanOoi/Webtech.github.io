57010e29ae65bf5ff6a7d4415ec71d89ab2a2675
Ooi Weishan <130117362+WeishanOoi@users.noreply.github.com>
2023-05-18 12:36:05 +0800

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,6 +1,6 @@
 function GetMap() { 
  
-    var maps = new Microsoft.Maps.Map(document.getElementById('maps'), { 
+    var map = new Microsoft.Maps.Map(document.getElementById('map'), { 
         center: new Microsoft.Maps.Location(3.140853, 101.693207), 
         zoom:11, 
         credentials: 'AmCdGM5M9Px2QVxkUld09IDlK3M2gKajjbYpCm58wbeelDCZscodbbgGT1xv_vgV',
